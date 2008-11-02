ae4c96dc4cd16c9e91cb22faa89598d6c59808a8
Mark Holton <holtonma@Macintosh-69.local>
2008-11-02 11:15:04 -0800

    cleaning out the extra crap

--- ogwr_scraper.rb
+++ ogwr_scraper.rb
@@ -8,6 +8,7 @@
 require 'rubygems'
 require 'hpricot'
 require 'open-uri'
+require 'ostruct'
 #require 'mysql'
 
 module OGWR
@@ -15,222 +16,23 @@ module OGWR
 
   class PageFetcher
     def fetch(ogwr_url)
-      #open(ogwr_url).read
-      Hpricot(open(ogwr_url))
-      #open('http://www.officialworldgolfranking.com/rankings/default.sps').read
-    end
-  end
-
-  class SlotMachine
-    # 
-    def initialize(num_gen)
-      @number_generator = num_gen
-      @cherries = []
-    end
-    
-    def pull_handle(num=3)
-      #another possible implementation (in class): [](0,3){ @number_generator.random_number }
-      num.times do
-        @cherries << @number_generator.random_number 
+      data = Hpricot(open(ogwr_url)).search("table:nth-child(5)")
+      players = [] #init
+      start_rank = -1 #(there are 2 empty trs to start the world ranking) #instead of a counter, I should extract this from page
+      (data/"td:nth-child(2)").each do |x|
+        playa = OpenStruct.new
+        playa.fname = x.search("a").inner_html.split(" ")[0]
+        playa.lname = x.search("a").inner_html.split(" ")[1]
+        playa.rank = start_rank
+        puts "#{playa.fname} #{playa.lname} #{playa.rank}"
+        players << playa
+        start_rank += 1
       end
-      @cherries
-    end
-  end
-  
-  class NumberGenerator
-    def initialize(number=10)
-      @number = number
-    end
-    
-    def random_number(limit=@number)
-      rand(limit)
-    end
-  end
-end
+      players.pop; players.reverse!; players.pop; players.pop; players.reverse! #remove one extra row at end, 2 extra at beginning
 
-
-
-class Leaderboard
-  def initialize file_or_url, root_path, leaderboard_path=""
-    @file_or_url = file_or_url #this smells
-    @root_path = root_path
-    @leaderboard_path = leaderboard_path
-    @leaders = []
-    @name = ""
-    @score = 0
-  end
-  
-  attr_accessor :root_path, :leaderboard_path, :leaders
-  
-  def populate
-    if (@file_or_url == "file")
-      file = open("test_index.html") { |f| Hpricot(f) }
-      doc = Hpricot(file.to_html)
-      elements = doc.search("/html/body//p")
-      #puts elements
-      all_rows = doc.search("//div[@class='leaderMain']/div/table/tbody/tr")
-    else
-      doc = Hpricot(open(@root_path + @leaderboard_path))
-      #all_rows = doc.search(".leaderMain/tr")
-      all_rows = doc.search("//.leaderMain/tr")
-    end
-
-    
-    all_rows.each do |row|
-      tds = (row/"td")
-      player = {}
-      
-      i = 0
-      tds.each do |col|
-        i += 1
-        #puts col
-        
-        #puts "#{i}"
-        #puts "col: #{col}"
-        
-        if i == 1
-          player[:pos] = col.inner_html
-          puts "pos: #{player[:pos]}"
-        end
-        if i == 3
-          if player[:pos] != "WD"
-          #puts "col: #{(col)}"
-          #puts "col/'a': #{(col/"a")}"
-          #puts "col/'a/img': #{(col/"a/img")}"
-          #puts "col//'a': #{(col/"//a")}"
-          #puts "col/'a:last': #{(col/"a:last")}"
-          if (col/"a:last") != nil
-            player[:name] = (col/"a:last").inner_html #(col/"a").inner_html
-            player[:name] = "Prayad Marksaeng" if player[:name] == nil
-            #puts player[:name]
-          else
-            player[:name] = ""
-            #puts "player blank"
-          end
-          #names = self.split_name(pla)
-          else
-            player[:name] = "WD"
-          end
-        end
-        if i == 4
-          player[:total] = col.inner_html
-          puts "total: #{player[:total]}"
-          @leaders << player
-          #update score here 
-        end
-        if i == 5
-          if player[:pos] != "WD"
-            player[:thru] = col.inner_html
-          else
-            player[:thru] = "-"
-          end
-          puts player[:thru]
-          puts "#{player[:name]}, #{player[:total]}, #{player[:thru]}, #{player[:pos]}"
-          if player[:name] != "WD"
-            self.update_db(player[:name], player[:total], player[:thru], player[:pos])
-          end
-        end
-        
-        #puts "added #{col}"
-        
-      end
-    end
-
-    return @leaders
-  end
-  
-  def split_name full_name
-    names = {}
-    split_names = full_name.split(" ")
-    if split_names.length == 3
-      names[:first] = split_names[0]
-      names[:last] = "#{split_names[1]} #{split_names[2]}" #jr., sr., III, etc
-    else
-      names[:first] = split_names[0]
-      names[:last] = split_names[1]
-    end
-    
-    names
-  end
-  
-  def update_db(scraped_name="- -", current_score="E", through="", pos="")
-   puts "scraped name: #{scraped_name}"
-   puts current_score
-     
-   first_name = self.split_name(scraped_name)[:first]
-   last_name = self.split_name(scraped_name)[:last]
-   
-   puts "first name: #{first_name}"
-   puts "last name: #{last_name}"
-   
-   if current_score == "E"
-     current_score = 0
-   else
-     current_score = current_score.to_i
-   end
-   puts "current_score: #{current_score}"
-   
-   #dbh = Mysql.real_connect("127.0.0.1", "root", "", "eyeon2_local_20080410")
-   dbh = Mysql.real_connect("65.36.177.230", "eyeon2", "pgachamp", "eyeon2") 
-   if first_name != nil || last_name != nil || first_name != "WD"
-     fn = dbh.escape_string(first_name)
-     ln = dbh.escape_string(last_name)
-     puts "#{through} --- through.length: #{through.length}"
-     if through != nil
-     if through.length <= 4
-       tru = through 
-     else
-       tru = "-"
-     end
-     end
-     str = "SELECT CurrentScoreRelPar, GolferFirstName, GolferLastName 
-      FROM tgolfer WHERE GolferFirstName = '#{fn}' 
-      AND GolferLastName = '#{ln}'"
-     # puts str
-     q_preupdate = dbh.query("SELECT CurrentScoreRelPar, GolferFirstName, GolferLastName 
-       FROM tgolfer WHERE GolferFirstName = '#{fn}' 
-       AND GolferLastName = '#{ln}'")
-     if q_preupdate.num_rows == 1
-       q_update = dbh.query("UPDATE tgolfer SET CurrentScoreRelPar = #{current_score}, 
-       thru = '#{tru}', position = '#{pos}'
-       WHERE GolferFirstName = '#{fn}' 
-       AND GolferLastName = '#{ln}'")
-       q_getupdate = dbh.query("SELECT CurrentScoreRelPar, GolferFirstName, GolferLastName 
-       FROM tgolfer WHERE GolferFirstName = '#{fn}' 
-       AND GolferLastName = '#{ln}'")
-       # q_getupdate.each do |row|
-       #  printf "%s, %s, %s\n", row[0], row[1], row[2]
-       # end
-       q_getupdate.free
-       q_preupdate.free
-     else
-       puts "************* no match with : #{scraped_name} ****************" # therefore inserting new..."
-       # q_insert = dbh.query("INSERT INTO tgolfer (GolferFirstName, 
-       #   GolferLastName, CurrentScoreRelPar, DegsofWallyVal, 
-       #   GolferImage, active, madecut, thru) 
-       #   VALUES ('#{first_name}', '#{last_name}', 0, 2, 'qualifier.gif', 1, 0, 0)")
-     end
-   end
-   dbh.close if dbh #disconnect
-  end
-  
-  
-  def update_all_leaders leaders
-    leaders.each do |player|
-      update_db(player[:player], player[:to_par], player[:thru])
+      players
     end
   end
-  
-  def to_screen leader_index
-    if leader_index < 1
-      @leaders.each do |player|
-        player.each {|k, v| puts "#{k} : #{v}" }
-      end
-    else
-      @leaders[leader_index].each{ |k, v| puts "#{k} : #{v}" }
-    end
-    
-  end 
   
 end
 
--- test_ogwr_scraper.rb
+++ test_ogwr_scraper.rb
@@ -8,71 +8,23 @@ require 'ostruct'
 
 class TestOGWR < Test::Unit::TestCase
   include OGWR
-
-  # test --- keep it from hitting the internet.  
-  def test_fetch_ogwr
-    
-    # extract fname and lname of players
-    data = Hpricot(open("http://www.officialworldgolfranking.com/rankings/default.sps")).search("table:nth-child(5)")
-    #data.pop; data.reverse!; data.pop; data.pop; data.reverse! #remove one extra row at end, 2 extra at beginning
-    players = [] #init
-    start_rank = -1 #(there are 2 empty trs to start the world ranking)
-    (data/"td:nth-child(2)").each do |x|
-      playa = OpenStruct.new
-      playa.fname = x.search("a").inner_html.split(" ")[0]
-      playa.lname = x.search("a").inner_html.split(" ")[1]
-      playa.rank = start_rank
-      puts "#{playa.fname} #{playa.lname} #{playa.rank}"
-      players << playa
-      start_rank += 1
-    end
-    players.pop; players.reverse!; players.pop; players.pop; players.reverse! #remove one extra row at end, 2 extra at beginning
-    
-    pp players
-    
-    pp players[30].lname
-    
-    # players = [] #init
-    # #ranking, strip any nbsp;
-    # data.each do |x|
-    #   playa = OpenStruct.new
-    #   #puts x.search("td:nth-of-type(1)").inner_html.strip.gsub(/&nbsp;/, "")
-    #   playa.rank = x.search("td:nth-of-type(1)").inner_html.strip.gsub(/&nbsp;/, "")
-    #   playa.fname = x.search("td:nth-of-type(3)").search("a").inner_html.strip.gsub(/&nbsp;/, "").split(" ")[0]
-    #   playa.lname = x.search("td:nth-of-type(3)").search("a").inner_html.strip.gsub(/&nbsp;/, "").split(" ")[1]
-    #   puts "Playa info --- #{playa.rank}::#{playa.fname} #{playa.lname}"
-    #   #puts x.search("td:nth-of-type(3)").search("a").inner_html.strip.gsub(/&nbsp;/, "")
-    # end
-    # 
-    # test = "&nbsp;12&nbsp;"
-    # result = test.strip.gsub(/&nbsp;/, "")
-    # puts "reg exp: #{result}"
-    
-    #grab ranking
-    # data = Hpricot(open("http://www.officialworldgolfranking.com/rankings/default.sps")).search("table:nth-child(5)")
-    # data.each do |x|; puts x.search("td:nth-of-type(n)"); end
-    #     
-      # puts x.search("td:nth-of-type(1)") #rank
-      # puts x.search("td:nth-of-type(2)") #name
-      # puts x.search("td:nth-of-type(4)") #pts avg
-      # puts x.search("td:nth-of-type(6)") #total points
-      # puts x.search("td:nth-of-type(8)") #num of events
-      # puts x.search("td:nth-of-type(10)") #total points
-
-  end
-  
-  def test_scrape_top50
-    fetcher = PageFetcher.new
-    doc = fetcher.fetch('http://www.officialworldgolfranking.com/rankings/default.sps')
-    #pp doc.search()
-  end
   
   def setup
     # # 10 = default.  
     # @topend = 10
     # @number_generator = NumberGenerator.new(@topend)
     # @slot_machine = SlotMachine.new(@number_generator)
+    
+    # i really should mock this out and not hit the network... later
   end
   
+  def test_scrape_top50
+    fetcher = PageFetcher.new
+    players = fetcher.fetch('http://www.officialworldgolfranking.com/rankings/default.sps') 
+    assert_equal 50, players.length
+    assert_equal 1, players[0].rank
+    assert_equal "Tiger", players[0].fname
+    assert_equal "Woods", players[0].lname
+  end
   
 end
