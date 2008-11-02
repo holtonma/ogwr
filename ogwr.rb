f6927904196868ccc37bc4c9ab2ee895a514d840
Mark Holton <holtonma@Macintosh-69.local>
2008-11-02 11:48:33 -0800

    calling it ogwr, not ogwr_scraper

---
+++ ogwr.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby -w
+# created by Mark Holton  (holtonma@gmail.com)
+# copy as much as you want to
+# 10-29-2008
+# purpose: scrape the official world golf ranking, and present it in a more usable form (Array of ostruct's)
+# using Hpricot, open-uri
+
+require 'rubygems'
+require 'hpricot'
+require 'open-uri'
+require 'ostruct'
+
+module OGWR
+  VERSION = '0.1.0'
+
+  class PageFetcher
+    def fetch(ogwr_url, page_num)
+      #page_num indicates range of 50n: page 1 >> 1-50, page 2 >> 51-100, page 3 >> 101-150...
+      data = Hpricot(open(ogwr_url)).search("table:nth-child(5)")
+      players = [] #init
+      start_rank = -1 + 50*(page_num-1) #(there are 2 empty trs to start the world ranking) #instead of a counter, I should extract this from page
+      (data/"td:nth-child(2)").each do |x|
+        playa = OpenStruct.new
+        playa.fname = x.search("a").inner_html.split(" ")[0]
+        playa.lname = x.search("a").inner_html.split(" ")[1]
+        playa.rank = start_rank
+        puts "#{playa.fname} #{playa.lname} #{playa.rank}"
+        players << playa
+        start_rank += 1
+      end
+      #clean this line up:
+      players.pop; players.reverse!; players.pop; players.pop; players.reverse! #remove one extra row at end, 2 extra at beginning
+
+      players
+    end
+  end
+  
+end
+
+
+
+
+
+
---
+++ test_ogwr.rb
@@ -0,0 +1,43 @@
+
+require 'ogwr'
+require 'rubygems'
+require 'test/unit'
+require 'flexmock/test_unit'
+require 'stringio'
+require 'ostruct'
+
+class TestOGWR < Test::Unit::TestCase
+  include OGWR
+  
+  def setup
+    # i really should mock this out and not hit the network... will do that later
+    # page = 1
+    # url = "http://www.officialworldgolfranking.com/rankings/default.sps?region=world&PageCount=#{page}"
+    # flexmock(fetcher).should_receive(:open).with(url, page).and_return{
+    #   File.open(sample.html, 'r') 
+    # }
+  end
+  
+  def test_scrape_top50
+    fetcher = PageFetcher.new
+    page = 1
+    url = "http://www.officialworldgolfranking.com/rankings/default.sps?region=world&PageCount=#{page}"
+    players = fetcher.fetch(url, page) 
+    assert_equal 50, players.length
+    assert_equal 1, players[0].rank
+    assert_equal "Tiger", players[0].fname
+    assert_equal "Woods", players[0].lname
+  end
+  
+  def test_scrape_251to300
+    fetcher = PageFetcher.new
+    page = 6
+    url = "http://www.officialworldgolfranking.com/rankings/default.sps?region=world&PageCount=#{page}"
+    players = fetcher.fetch(url, page) 
+    assert_equal 50, players.length
+    assert_equal 251, players[0].rank
+    assert_equal "Ignacio", players[0].fname
+    assert_equal "Garrido", players[0].lname
+  end
+  
+end
